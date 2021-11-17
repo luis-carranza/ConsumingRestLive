26edb7616b1cd1541bed4a68aaa3a247d9fca73e
Luis Carranza <carranlu@gmail.com>
2021-11-16 19:27:38 -0600

    Initial Commit

---
+++ ConsumingRestLive/ConsumingRestLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumingRestLiveApp.swift
+//  ConsumingRestLive
+//
+//  Created by Luis Javier Carranza on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ConsumingRestLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConsumingRestLive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConsumingRestLive
+//
+//  Created by Luis Javier Carranza on 16/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
